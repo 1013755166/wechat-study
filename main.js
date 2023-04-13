faa8ce212a2619164a28292f71cafc215d214aae
1013755166 <1013755166@qq.com>
2023-04-13 20:08:29 +0800

    home seccess

--- main.js
+++ main.js
@@ -1,20 +1,47 @@
-
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+import {
+  $http
+} from '@escook/request-miniprogram'
+
+uni.$http = $http
+
+// 配置请求根路径
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
+// 请求拦截器，请求开始前做的一些事
+$http.beforeRequest = function(options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+// 响应拦截器，请求完成后做的一些事
+$http.afterRequest = function() {
+  uni.hideLoading()
+}
+// 封装的展示消息提示方法
+uni.$showMsg = function(title = '数据加载失败', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+  ...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+  createSSRApp
+} from 'vue'
 import App from './App.vue'
 export function createApp() {
   const app = createSSRApp(App)
